fa548dc1ca2bb88d60bdaf9302a6864fd7eea827
853219061 <853219061@qq.com>
2023-01-09 15:51:56 +0800

    feat: 处理首页

---
+++ src/components/Loading/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="loading-container">
+    <img src="@/assets/loading.svg" alt="">
+  </div>
+</template>
+<script>
+export default {
+  name: 'APP',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  created () {
+  },
+  mounted () {
+  },
+}
+</script>
+<style lang="less" scoped>
+@import '~@/styles/mixin.less';
+.loading-container{
+    
+    .self-center();                     
+    
+}
+</style>
